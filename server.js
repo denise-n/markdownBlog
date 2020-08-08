2e73211ad4e009824c22b88d452902b255a72292
denise-n <denise.ndalamba@gmail.com>
2020-08-07 22:41:10 -0300

    change db to mongo atlas

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
+const path = require('path')
 const express = require('express')
 const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+const connectDB = require('./config/db')
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
 const methodOverride = require('method-override')
 const app = express()
 
-mongoose.connect('mongodb://localhost/blog', 
-    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+// Load config
+dotenv.config({path: './config/config.env'})
+
+//connect to database
+connectDB()
 
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
 
+// Routes
 app.get('/', async (req, res) => {
     const articles = await Article.find().sort({ createdAt: 'desc' })
     res.render('articles/index', { articles: articles })
 })
 ,
-app.listen(5000)
-
 app.use('/articles', articleRouter)
+
+// Listen for port
+const PORT = process.env.PORT || 5000 
+
+app.listen(PORT, console.log(`Server running on port ${PORT}`))
